28a67b408cae1154fb5adee1b9f79c6cf8dd7191
A-Baji <bidabaji@gmail.com>
2022-04-05 12:30:56 -0500

    bump size limit

--- tests/main.sh
+++ tests/main.sh
@@ -32,21 +32,21 @@ SHELL_CMD_TEMPLATE="docker run --rm -i \$SHELL_CMD_FLAGS $REF \
 	$([ ${DISTRO} == 'debian' ] && echo bash || echo sh) -c"
 # determine reference size
 if [ $DISTRO == alpine ] && [ $PY_VER == '3.10' ]; then
-	SIZE_LIMIT=990
+	SIZE_LIMIT=991
 elif [ $DISTRO == alpine ] && [ $PY_VER == '3.9' ]; then
-	SIZE_LIMIT=702
+	SIZE_LIMIT=723
 elif [ $DISTRO == alpine ] && [ $PY_VER == '3.8' ]; then
-	SIZE_LIMIT=661
+	SIZE_LIMIT=682
 elif [ $DISTRO == alpine ] && [ $PY_VER == '3.7' ]; then
-	SIZE_LIMIT=669
+	SIZE_LIMIT=693
 elif [ $DISTRO == debian ] && [ $PY_VER == '3.10' ]; then
 	SIZE_LIMIT=1210
 elif [ $DISTRO == debian ] && [ $PY_VER == '3.9' ]; then
-	SIZE_LIMIT=940
+	SIZE_LIMIT=962
 elif [ $DISTRO == debian ] && [ $PY_VER == '3.8' ]; then
-	SIZE_LIMIT=893
+	SIZE_LIMIT=915
 elif [ $DISTRO == debian ] && [ $PY_VER == '3.7' ]; then
-	SIZE_LIMIT=896
+	SIZE_LIMIT=912
 fi
 SIZE_LIMIT=$(echo "scale=4; $SIZE_LIMIT * 1.02" | bc)
 # verify size minimal
