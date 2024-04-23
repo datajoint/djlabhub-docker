e9f8c8c941fa469fc205c0c337a89614586c4feb
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-04-23 11:28:44 -0500

    debug: remove pip install init script

--- singleuser/config/before_start_hook.sh
+++ singleuser/config/before_start_hook.sh
@@ -8,18 +8,18 @@ yq '.properties.defaultViewers.default = {"markdown":"Markdown Preview"}' \
 
 # clone and install DJLABHUB_REPO or DJLABHUB_REPO_SUBPATH
 # for private repo, include PAT(Personal Access Token) in the https url
-if [[ ! -z "${DJLABHUB_REPO}" ]]; then
-  REPO_NAME=$(basename $DJLABHUB_REPO | sed 's/.git//')
+# if [[ ! -z "${DJLABHUB_REPO}" ]]; then
+#   REPO_NAME=$(basename $DJLABHUB_REPO | sed 's/.git//')
 
-  echo "INFO::Cloning repo $DJLABHUB_REPO"
-  git clone $DJLABHUB_REPO $HOME/$REPO_NAME
-  if [[ ! -z "${DJLABHUB_REPO_BRANCH}" ]]; then
-    echo "INFO::Switch to branch $DJLABHUB_REPO_BRANCH"
-    git -C $HOME/$REPO_NAME switch $DJLABHUB_REPO_BRANCH
-  fi
+#   echo "INFO::Cloning repo $DJLABHUB_REPO"
+#   git clone $DJLABHUB_REPO $HOME/$REPO_NAME
+#   if [[ ! -z "${DJLABHUB_REPO_BRANCH}" ]]; then
+#     echo "INFO::Switch to branch $DJLABHUB_REPO_BRANCH"
+#     git -C $HOME/$REPO_NAME switch $DJLABHUB_REPO_BRANCH
+#   fi
 
-  if [[ $DJLABHUB_REPO_INSTALL == "TRUE" ]]; then
-    echo "INFO::Installing repo"
-    pip install -e $HOME/$REPO_NAME
-  fi
-fi
+#   if [[ $DJLABHUB_REPO_INSTALL == "TRUE" ]]; then
+#     echo "INFO::Installing repo"
+#     pip install -e $HOME/$REPO_NAME
+#   fi
+# fi
