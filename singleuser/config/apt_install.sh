6ddcec7df9a8743bebe09486c52a3fabd77e5661
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-07-12 18:28:48 -0500

    Docker build working

--- singleuser/config/apt_install.sh
+++ singleuser/config/apt_install.sh
@@ -3,13 +3,14 @@
 sudo apt update
 
 # Install
-sudo apt-get install mysql-client gcc nodejs npm -y --no-install-recommends
+sudo apt-get install mysql-client gcc-12 build-essential pkg-config libsecret-1-dev nodejs npm -y --no-install-recommends
 sudo apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 # Install Yarn
 # npm install --global yarn
 corepack enable
+npm i -g node-gyp
 
 # Other installation
 wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
