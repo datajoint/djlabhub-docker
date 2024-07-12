e7316e6543966f53eeb75f7822eb38bd08cfe914
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-07-12 17:06:09 -0500

    First pass at theia installation
    Working from https://jupyter-server-proxy.readthedocs.io/en/latest/convenience/packages/theia.html

--- singleuser/config/apt_install.sh
+++ singleuser/config/apt_install.sh
@@ -3,9 +3,13 @@
 sudo apt update
 
 # Install
-sudo apt-get install mysql-client gcc -y --no-install-recommends
+sudo apt-get install mysql-client gcc nodejs npm -y --no-install-recommends
 sudo apt-get clean
 rm -rf /var/lib/apt/lists/*
+
+# Install Yarn
+# npm install --global yarn
+corepack enable
 
 # Other installation
 wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
